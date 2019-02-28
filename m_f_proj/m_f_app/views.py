8708bf88916fd99d29fee2552c57d314c2593c15
Kevin Yancy <kevin@code-crew.org>
2019-02-28 13:35:56 -0600

    init

--- m_f_proj/m_f_app/views.py
+++ m_f_proj/m_f_app/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 
 # This view handles the landing page
 from .forms import NewUserForm
+from .models import User
 
 
 def index(request):
-    return render(request, 'm_f_app/index.html')
+    allusers = User.objects.all()
+    return render(request, 'm_f_app/index.html', {'user_list': allusers})
 
 
 # This page will provide a form to add users
